848865136adc5ea1b53c4847f336afc0a373e4e5
jiangzhenjie <zhenjie_jiang@foxmail.com>
2018-07-15 11:03:17 +0800

    initial client project

---
+++ SSOLoginClient/SSOLoginClient/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SSOLoginClient
+//
+//  Created by jiangzhenjie on 2018/7/15.
+//  Copyright © 2018年 JiangZhenjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SSOLoginClient/SSOLoginClient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SSOLoginClient
+//
+//  Created by jiangzhenjie on 2018/7/15.
+//  Copyright © 2018年 JiangZhenjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
